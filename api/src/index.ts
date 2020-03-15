c1497026d92dbb91cea6a7dd0422105e979b43d4
Elias <eliasgarciadev@gmail.com>
2020-03-15 21:01:17 +0100

    Initial commit for the api subproject

---
+++ api/src/index.ts
@@ -0,0 +1,19 @@
+import * as express from "express";
+
+function main(): void {
+  const app: express.Express = express();
+
+  app.get(
+    "",
+    (_: express.Request, res: express.Response, next: express.NextFunction): express.Response => {
+      return res.status(200).json({ hello: "world" });
+    }
+  )
+
+  app.listen(3000, () => {
+    // tslint:disable-next-line: no-console
+    console.log(`API listening on port ${3000}`);
+  });
+}
+
+main();
